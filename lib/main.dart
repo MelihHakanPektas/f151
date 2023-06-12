dfd6cb7c62e8f4902818fddfd3e17bf81007f1c5
Melih Hakan Pektas <melihhakanpektas@gmail.com>
2023-06-12 15:53:45 +0300

    Bloc - Firebase - Basic Structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,17 @@
+import 'package:f151/bloc/app_info_bloc.dart';
+import 'package:f151/constants/app_colors.dart';
+import 'package:f151/firebase_options.dart';
+import 'package:f151/pages/wrapper.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MainApp());
 }
 
@@ -9,12 +20,32 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+    return MultiBlocProvider(
+      providers: [BlocProvider(create: (context) => AppInfoBloc())],
+      child: MaterialApp(
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(
+              scaffoldBackgroundColor: const Color(0xFFeff1f7),
+              brightness: Brightness.light,
+              colorSchemeSeed: Colors.deepPurpleAccent,
+              elevatedButtonTheme: ElevatedButtonThemeData(
+                  style: ElevatedButton.styleFrom(
+                      minimumSize: const Size(double.infinity, 45),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20)))),
+              appBarTheme:
+                  const AppBarTheme(backgroundColor: kAppBarBackgroundColor)),
+          darkTheme: ThemeData(
+            brightness: Brightness.dark,
+            colorSchemeSeed: Colors.deepPurpleAccent,
+            elevatedButtonTheme: ElevatedButtonThemeData(
+                style: ElevatedButton.styleFrom(
+                    minimumSize: const Size(double.infinity, 45),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20)))),
+          ),
+          home: const Wrapper()),
     );
   }
 }
